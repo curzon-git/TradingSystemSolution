29e3bd490bac51b501031008967e4145bfcf0294
curzon-git <zafrul.bhuia@curzonsecurities.com>
2025-08-17 12:05:00 +0100

    Check new additions .. UiState / Event

---
+++ TradingWebInterface/TradingUI.Contracts/UiEvent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TradingUi.Contracts
+{
+    public class UiEvent
+    {
+        public string Name { get; set; } = "";
+        public Dictionary<string, string> Args { get; set; } = new();
+    }
+}
---
+++ TradingWebInterface/TradingUI.Contracts/UiState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TradingUi.Contracts
+{
+    public class UiState
+    {
+        public Dictionary<string, string> Fields { get; set; } = new();
+        public Dictionary<string, List<Dictionary<string, object>>> Tables { get; set; } = new();
+        public Dictionary<string, object> Meta { get; set; } = new();
+    }
+}
